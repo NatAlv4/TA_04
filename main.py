025f6ca4e551c1d4707a0e12e4b270450843a604
Natalia Álvarez <>
2021-04-17 19:49:06 +0000

    Creacion de la interfaz de el registro de usuarios

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #se agrego flask
 from flask import Flask, render_template, send_file
+
 app = Flask('app')
 
 #creación de rutas
@@ -13,5 +14,7 @@ def index():
 def about():
   return render_template("about.html")
 
-
+@app.route('/login')
+def login():
+  return render_template("login.html")
 app.run(host='0.0.0.0', port=8080, debug=True)
