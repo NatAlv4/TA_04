b0eca491ffaff81f0cebe0955d0b8d5d6c864216
Juan José Sánchez Mejía <>
2021-04-20 03:44:04 +0000

    Interfaz Registro de usuario

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def index():
  return render_template ("index.html")
 
 
-
 @app.route('/about')
 def about():
   return render_template("about.html")
@@ -17,4 +16,14 @@ def about():
 @app.route('/login')
 def login():
   return render_template("login.html")
+
+@app.route('/sign_up')
+def sign_up():
+  return render_template("sign_up.html")
+
+@app.route('/Pswrd_R')
+def password():
+  return render_template("Password_Recovery.html")
+
+  
 app.run(host='0.0.0.0', port=8080, debug=True)
